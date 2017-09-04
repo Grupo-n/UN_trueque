7477e95d08c8426716a801de653993b3beb5fe65
jairoasuarez <jaasuarezga@gmail.com>
2017-09-03 23:03:10 -0500

    Login y registro incorporados con gema devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
   root 'welcome#index'
   get 'welcome/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-=======
-  get 'welcome/index'
-
-  root 'welcome#index'
->>>>>>> d64600b0208593dcd84d287b842ae821af0830e9
 end
