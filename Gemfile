22e444464e9ca7ea134511be982168e8bed5f37e
Jonathan Granados <joagranadosme@unal.edu.co>
2017-09-28 17:17:33 +0000

    Adding locales

--- Gemfile
+++ Gemfile
@@ -45,6 +45,8 @@ gem 'omniauth-facebook'
 
 gem 'bootstrap', '~> 4.0.0.alpha'
 
+gem 'rails-i18n', '~> 5.0.0' # For 5.0.x and 5.1.x
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_locale
   #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!, except: [:index, :contact, :new]
@@ -7,6 +8,10 @@ class ApplicationController < ActionController::Base
   #def after_sign_in_path_for(resource)
   #  '/static_pages/principal'
   #aend
+  
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :avatar, :avatar_cache])
