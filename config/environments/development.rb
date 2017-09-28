7f1fff1ba04f61b0df3fd58f73acf2158039b7a2
jairoasuarez <jaasuarezga@gmail.com>
2017-09-28 12:36:07 -0500

    Arreglo archivo development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -65,21 +65,4 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
-
-
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    user_name:      'un.truequeapp@gmail.com',
-    password:       'untrueque2017',
-    domain:         'localhost:3000',
-    address:       'smtp.gmail.com',
-    port:          '587',
-    authentication: :plain,
-    enable_starttls_auto: true
-  }
-
-
 end
