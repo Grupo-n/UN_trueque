d7fd8e9cfbadd64bbe2725c0ddf5cc18524a60ae
Jonathan Granados <joagranadosme@unal.edu.co>
2017-09-28 05:08:23 +0000

    Setting locale

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20170927221011) do
 
   create_table "products", force: :cascade do |t|
@@ -30,9 +29,6 @@ ActiveRecord::Schema.define(version: 20170927221011) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-=======
-ActiveRecord::Schema.define(version: 20170922160706) do
->>>>>>> github/miguelC-Spring2
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -45,13 +41,6 @@ ActiveRecord::Schema.define(version: 20170922160706) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
-    t.string "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string "unconfirmed_email"
-    t.integer "failed_attempts", default: 0, null: false
-    t.string "unlock_token"
-    t.datetime "locked_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "first_name"
