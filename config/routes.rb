ecc1d7dcca6f4ffb5654fbfdabacabf888b261e7
Jonathan Granados <joagranadosme@unal.edu.co>
2017-09-28 05:04:48 +0000

    Fixing merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,14 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
   resources :transactions
   resources :money
   resources :products
   resources :services
   resources :items
   resources :people
+  
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
-=======
-
-  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
-
->>>>>>> github/miguelC-Spring2
+  
   root :to => "welcome#new"
   get 'welcome/index'
   get 'static_pages/principal'
