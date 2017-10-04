4677564c6f4de1e196d587917973887e4ca21687
Miguel Cortes <macortesn@unal.edu.co>
2017-10-04 18:41:36 -0500

    Correcion de errores del login, implementacion de vista para perfil y para editar perfil

--- config/routes.rb
+++ config/routes.rb
@@ -4,14 +4,18 @@ Rails.application.routes.draw do
   resources :products
   resources :services
 
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   root :to => "welcome#new"
   get 'welcome/index'
   get 'static_pages/principal' , :as => :user_home
+
+  get 'user/view-profile', :to =>'static_pages#profileInformation', :as => :user_information
+
   as :user do
-    get 'users/profile', :to => 'devise/registrations#edit', :as => :user_edit
+    get 'user/edit-profile', :to => 'devise/registrations#edit', :as => :user_edit
   end
+
   get 'contact', to: 'messages#new', as: 'contact'
   post 'contact', to: 'messages#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
