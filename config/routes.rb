03263673743846782bc99c8c291621bba928c617
Jonathan Granados <joagranadosme@unal.edu.co>
2017-10-04 20:52:18 +0000

    Adding Product

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
 
 
+  resources :products
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
 
   root :to => "welcome#new"
