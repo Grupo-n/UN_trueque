e85cdec339bd99100a24c28dff33a5939ed7c282
jairoasuarez <jaasuarezga@gmail.com>
2017-10-05 15:18:09 -0500

    Gema annotate

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: services
+#
+#  id          :integer          not null, primary key
+#  name        :string
+#  description :string
+#  duration    :time
+#  typeInt     :integer
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Service < ApplicationRecord
     
     belongs_to :user, optional:true
