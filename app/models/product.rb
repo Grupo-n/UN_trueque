5387f3427da0c2d2f35ea7acea0c504677f7baad
Jonathan Granados <joagranadosme@unal.edu.co>
2017-10-06 21:52:39 +0000

    Adding validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,4 +2,8 @@ class Product < ApplicationRecord
     
     belongs_to :user
     
+    validates :name, presence: true
+    validates :t_product, presence: true
+    validates :description, presence: true, length: {maximum: 100}
+    
 end
