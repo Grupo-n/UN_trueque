a5f6d80264e94e57f5a0f452433685b85acd9a26
Jonathan Granados <joagranadosme@unal.edu.co>
2017-10-06 21:14:03 +0000

    Fixing routes after merge

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
 
   resources :products
-<<<<<<< HEAD
   resources :services
 
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-=======
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'settings' }
-  
->>>>>>> ProductsFixing
   root :to => "welcome#new"
   
   get 'my_products/index'
