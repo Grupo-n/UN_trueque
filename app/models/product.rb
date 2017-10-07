aca14fb75aebc955a7788aefbd4e56c7d4dea3df
Jonathan Granados <joagranadosme@unal.edu.co>
2017-10-07 22:27:19 +0000

    Testing transaction

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,21 @@ class Product < ApplicationRecord
     validates :p_type, presence: true
     validates :description, presence: true, length: {maximum: 100}
     
+    def make_transaction(other)
+        self.name = "Transaction done"
+        other.name = "Transaction done"
+        Product.transaction do
+            raise "Transaction failed" unless self.save && other.save
+        end
+    end
+    
+    private
+        def transaction_success
+            Logger.info "Transfer succed for Product #{self.to_param}"
+        end
+        
+        def transaction_failed
+            Legger.warn "Transfer failed for Product #{self.to_param}"
+        end
+        
 end
