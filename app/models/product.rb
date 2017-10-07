af2929cf284b67f7ebeb746f51ed3fe9864afd69
Miguel Cortes <macortesn@unal.edu.co>
2017-10-06 23:03:34 -0500

    Agragando imagenes a modelos nuevos, modificado fron de las diferentes vistas de producto

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,9 +1,12 @@
 class Product < ApplicationRecord
-    
+
     belongs_to :user
-    
+
     validates :name, presence: true
     validates :p_type, presence: true
     validates :description, presence: true, length: {maximum: 100}
-    
+
+    has_attached_file :product_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/missing.png"
+    validates_attachment_content_type :product_image, content_type: /\Aimage\/.*\z/
+
 end
