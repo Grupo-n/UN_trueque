9a35339d2e0c30a3777f142fa58891afbef48f24
jairoasuarez <jaasuarezga@gmail.com>
2017-10-11 12:06:03 -0500

    Paginacion pagina principal

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,11 +1,13 @@
 class StaticPagesController < ApplicationController
-  
+
   def principal
     @products = Product.all
+    #@products = Product.where(:condicion_ejemplo => 1).order("created_at DESC").paginate(:page => params[:page]), :per_page => 10)
+    @products = Product.paginate(:page => params[:page], :per_page => 9)
   end
 
   def profileInformation
     render '/static_pages/profileInformation'
   end
-  
+
 end
