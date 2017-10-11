9cbda0df6e2838ac2f68aeedf1c03fbc89599936
jairoasuarez <jaasuarezga@gmail.com>
2017-10-11 09:30:40 -0500

    Configuracion variables de entorno para produccion

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -105,20 +105,20 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  
+
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.default_url_options = { host: "untruequetest.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      'un.truequeapp@gmail.com',
-    password:       'untrueque2017',
-    domain:         'localhost:3000',
+    user_name:      ENV['GMAIL_USERNAME'],
+    password:       ENV['GMAIL_PASSWORD'],
+    domain:         "untruequetest.herokuapp.com",
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
     enable_starttls_auto: true
   }
-  
-  
+
+
 end
