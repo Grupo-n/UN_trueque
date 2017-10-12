0817749904256130aa5cf2da1c705a4b3f892708
Jonathan Granados <joagranadosme@unal.edu.co>
2017-10-12 21:28:05 +0000

    Routing domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -131,7 +131,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     user_name:      ENV['GMAIL_USERNAME'],
     password:       ENV['GMAIL_PASSWORD'],
-    domain:         "untruequetest.herokuapp.com",
+    domain:         "untrueque.herokuapp.com",
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
