e7d8b3644d90fc20e2318bfc809ba7f912751b85
Jonathan Granados <joagranadosme@unal.edu.co>
2017-10-12 17:13:41 +0000

    .

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -129,8 +129,8 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: "untrueque.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-    user_name:      ENV['GMAIL_USERNAME'],
-    password:       ENV['GMAIL_PASSWORD'],
+    user_name:      "un.truequeapp@gmail.com",
+    password:       "untrueque2017",
     domain:         "untrueque.herokuapp.com",
     address:       'smtp.gmail.com',
     port:          '587',
