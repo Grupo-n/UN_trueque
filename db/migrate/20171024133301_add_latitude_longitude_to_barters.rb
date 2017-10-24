98a2cbf49742ab20107a8d00aabaa548d3154040
Juan Nicolas Sastoque <jnsastoquee@unal.edu.co>
2017-10-24 14:10:46 +0000

    definido defecto latitud y longitud

--- db/migrate/20171024133301_add_latitude_longitude_to_barters.rb
+++ db/migrate/20171024133301_add_latitude_longitude_to_barters.rb
@@ -1,10 +1,10 @@
 class AddLatitudeLongitudeToBarters < ActiveRecord::Migration[5.1]
   def change
     unless column_exists? :barters, :latitude
-      add_column :barters, :latitude, :float
+      add_column :barters, :latitude, :float, default: 4.635487
     end
     unless column_exists? :barters, :latitude
-      add_column :barters, :longitude, :float
+      add_column :barters, :longitude, :float, default: -74.082719
     end
   end
 end
