565d1d6a81ec9c3929f4171c87b226b5455f30e2
Miguel Cortes <macortesn@unal.edu.co>
2017-10-24 22:13:31 -0500

    Prueba de login con fb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,11 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
-    
+
     @user = User.from_omniauth(request.env["omniauth.auth"])
+    #puts "user"
+    #puts @user
+    puts @user.errors.to_a
+    #puts request.env["omniauth.auth"]
 
     if @user.persisted?
       sign_in_and_redirect @user, :event => :authentication
