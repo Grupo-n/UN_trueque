4520a7bae6e3cfd5fbcf0bbaf409c314f871342f
jairoasuarez <jaasuarezga@gmail.com>
2017-10-26 16:22:32 -0500

    Arreglo host production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -126,7 +126,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: "untrueque2.herokuapp.com"}
+  config.action_mailer.default_url_options = { host: "untrueque.herokuapp.com"}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      ENV['GMAIL_USERNAME'],
