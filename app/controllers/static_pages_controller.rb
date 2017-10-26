0574a596b441d2210be4eaa2848e5696a3bb57de
joagranadosme <joagranadosme@unal.edu.co>
2017-10-26 16:47:16 -0500

    adding category

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,6 +3,7 @@ class StaticPagesController < ApplicationController
 before_action :search
 
   def principal
+    @categories = Category.all
     @search = Product.descendent.search(params[:q])
     @products = @search.result.paginate(:page => params[:page], :per_page => 12)
   end
