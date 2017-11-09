97d4e17be8448dc2f28c726d5a217a6410b69052
jairoasuarez <jaasuarezga@gmail.com>
2017-11-09 10:09:02 -0500

    .

--- config/routes.rb
+++ config/routes.rb
@@ -70,33 +70,4 @@ Rails.application.routes.draw do
 
   #Generar factura transaccion completa
   get 'my_products/:id/succesfull_transaction/:hash' => "my_products#generate_barter_pdf", as: 'generate_facture'
-=begin
-  #Productos usuario
-  get 'my_products/index'
-  get 'my_products/myobjects'
-  get 'my_products/myservices'
-  get 'my_products/offer'
-  get 'my_products/mytransactions' => "my_products#mytransactions", as: 'mytransactions'
-  get 'my_products/offers_made'
-  get 'my_products/offers_received'
-
-  get 'products/:id/show' => "products#show", as: 'show'
-  get 'products/:id/offer' => "products#offer", as: 'offer'
-  #get 'statistics/products', :to => "products#statistics", :as => :products_statistics
-  get 'my_products/:id/offer' => "my_products#offer", as: 'my_offer'
-  get 'my_products/:id/accept' => "my_products#accept", as: 'accept'
-  get 'my_products/:id/change_ubication' => "barters#change_ubication", as: 'change_ubication'
-
-
-
-
-  #Pagina principal
-  #get 'welcome/index'
-  #get 'static_pages/principal' , :as => :user_home
-  get 'my_products/myobjects' , :as => :myObjects
-  get 'my_products/myservices' , :as => :myServices
-  get 'my_products/index' , :as => :myObjectsServices
-  get 'my_products/myposts' , :as => :myPosts
-
-=end
   end
