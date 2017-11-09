9a4df40b738f9a1972cb62da58798ee1bd61bc7a
jairoasuarez <jaasuarezga@gmail.com>
2017-11-09 11:04:51 -0500

    Correcion de maps intento 4

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require gmaps/google
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
@@ -23,5 +24,4 @@
 //= require Chart.bundle
 //= require chartkick
 //= require underscore
-//= require gmaps/google
 //= require_tree .
