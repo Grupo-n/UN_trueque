08aa54f4cbc679dfbbc4fc268d7d7e196da783ea
Juan Nicolas Sastoque <jnsastoquee@unal.edu.co>
2017-11-09 15:10:42 +0000

    gema para pruebas

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -2,11 +2,12 @@ require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
   test "should register new user" do
-    get "/welcome/index"
-    assert_response :success
+    get user_registration_path #rails routes
+    #assert_response :success
     
-    #post "/", params: {user: {first_name: " " , last_name: " ", email: " ", password: " ", password_confirmation: " "}}
+    post user_registration_path, params: {user: {first_name: " " , last_name: " ", email: " ", password: " ", password_confirmation: " "}}
     
-    #assert_redirected_to "/"
+    #assert_redirected_to user_registration_path
+    assert_template 'devise/registration/new'
   end
 end
