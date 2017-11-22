7250c92033f1381a04a987dd84eb93a234616142
joagranadosme <joagranadosme@unal.edu.co>
2017-11-22 18:56:55 -0500

    .

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_search
-    @search = Product.descendent.search(params[:q])
+    @search = Product.descendent(current_user).search(params[:q])
     if params[:search]
       search_params = CGI::escapeHTML(params[:search])
       redirect_to ("/static_pages/principal?utf8=%E2%9C%93&search=#{search_params}")
