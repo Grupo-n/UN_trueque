4554e7f6eb9bc83bc8cd7dfaacdf2518df4df5a1
joagranadosme <joagranadosme@unal.edu.co>
2017-11-22 18:54:08 -0500

    Fixing in search bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,7 +21,7 @@ class ApplicationController < ActionController::Base
   end
 
   def set_search
-    @search = Product.where.not(user_id: current_user.id).search(params[:q])
+    @search = Product.descendent.search(params[:q])
     if params[:search]
       search_params = CGI::escapeHTML(params[:search])
       redirect_to ("/static_pages/principal?utf8=%E2%9C%93&search=#{search_params}")
