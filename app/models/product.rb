e9272b134bb410e31e927c26e028a72ce0da23fe
joagranadosme <joagranadosme@unal.edu.co>
2017-11-22 18:24:26 -0500

    Inverting order in my products and services

--- app/models/product.rb
+++ app/models/product.rb
@@ -28,7 +28,7 @@ class Product < ApplicationRecord
     validates :name, presence: true
     validates :p_type, presence: true
     validates :description, presence: true, length: {maximum: 100}
-    
+
     has_attached_file :product_image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "missing.png"
     validates_attachment_content_type :product_image, content_type: /\Aimage\/.*\z/
 
@@ -45,11 +45,11 @@ class Product < ApplicationRecord
     end
 
     def self.objects
-      return Product.where("p_type = ? AND available = ?", 1, true)
+      return Product.where("p_type = ? AND available = ?", 1, true).order('created_at DESC')
     end
 
     def self.services
-      return Product.where("p_type = ? AND available = ?", 2, true)
+      return Product.where("p_type = ? AND available = ?", 2, true).order('created_at DESC')
     end
 
 end
