e34d7cdd08647211cf65253c7f014daebea3b213
joagranadosme <joagranadosme@unal.edu.co>
2017-11-22 20:59:17 -0500

    Fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,23 @@
 require 'faker'
 
 User.create(email: "joagranadosme@unal.edu.co", first_name: "Jonathan",
-  last_name: "Granados Méndez", password: "12345678",
+  last_name: "Granados Méndez", password: "Un123456",
    password_confirmation: "Un123456", score: 5.0)
 
 User.create(email: "gsuarezc@unal.edu.co", first_name: "Gabriela",
-  last_name: "Suárez Carvajal", password: "12345678",
+  last_name: "Suárez Carvajal", password: "Un123456",
    password_confirmation: "Un123456", score: 5.0)
 
 User.create(email: "jaasuarezga@unal.edu.co", first_name: "Jairo",
-  last_name: "Suárez Garnica", password: "12345678",
+  last_name: "Suárez Garnica", password: "Un123456",
    password_confirmation: "Un123456", score: 5.0)
 
 User.create(email: "macortesn@unal.edu.co", first_name: "Miguel Angel",
-  last_name: "Cortes Nivia", password: "12345678",
+  last_name: "Cortes Nivia", password: "Un123456",
    password_confirmation: "Un123456", score: 5.0)
 
 User.create(email: "jnsastoquee@unal.edu.co", first_name: "Nicolas",
-  last_name: "Sastoque", password: "12345678",
+  last_name: "Sastoque", password: "Un123456",
    password_confirmation: "Un123456", score: 5.0)
 
 User.all.each do |d|
